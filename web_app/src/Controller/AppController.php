6ff2c732ee3532c7a6875c11099d7a99f3c94cbb
pjpawel <pawel.jan.podgorski@gmail.com>
2023-01-18 21:41:06 +0100

    Add components to controller

--- web_app/src/Controller/AppController.php
+++ web_app/src/Controller/AppController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Config;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
@@ -12,7 +16,8 @@ class AppController extends AbstractController
     #[Route('', name: 'index', methods: 'GET')]
     public function index(): Response
     {
-        $photo = false;
+        $photo = $this->photoInfo($this->getPhotoPath());
+
         return $this->render('index.html.twig', [
             'latitude' => '52.19712139157508',
             'longitude' => '21.191097581472455',
@@ -20,24 +25,38 @@ class AppController extends AbstractController
         ]);
     }
 
+    private function photoInfo(string $path): bool
+    {
+        if (file_exists($path) && is_file($path)) {
+            if (stat($path)['mtime'] + 30 < time()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function getPhotoPath(): string
+    {
+        return $this->getParameter('kernel.project_dir') . Config::getPhotoPath();
+    }
+
     #[Route('photo', name: 'photo', methods: 'GET')]
     public function photo(): Response
     {
-        $data = [
-            'latitude' => '52.19712139157508',
-            'longitude' => '21.191097581472455'
-        ];
-        return $this->json($data);
+        $path = $this->getPhotoPath();
+        if (!$this->photoInfo($path)) {
+            throw new NotFoundHttpException();
+        }
+
+        return new BinaryFileResponse($path);
     }
 
     #[Route('photo-upload', name: 'photo_upload', methods: 'POST')]
-    public function photoUpload(): Response
+    public function photoUpload(Request $request): Response
     {
-        $data = [
-            'latitude' => '52.19712139157508',
-            'longitude' => '21.191097581472455'
-        ];
-        return $this->json();
+        $file = $request->files->get('file');
+        var_dump($file);
+        return $this->json([]);
     }
 
 }
