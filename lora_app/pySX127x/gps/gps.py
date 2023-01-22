88291818a1fd484142a990bf7a0ada933594beab
Piotr Socha <piotrsocha01@gmail.com>
2023-01-22 22:38:24 +0100

    Fix import

--- lora_app/pySX127x/gps/gps.py
+++ lora_app/pySX127x/gps/gps.py
@@ -1,15 +1,14 @@
 import serial
 from time import sleep
-from parser import GPGGAParser
+from .parser import GPGGAParser
 
 s = serial.Serial("/dev/ttyS0")
 s.baudrate = 9600
 
+
 def get_coords():
-    while(True):
+    while True:
         d = s.readline().decode()
         if d.startswith("$GPGGA"):
             p = GPGGAParser(d)
-            return(p.latitude, p.longitude)
-
-
+            return (p.latitude, p.longitude)
